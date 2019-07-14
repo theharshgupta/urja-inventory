5c02bc0bd681f96b015f8622f5a78ce832d1c9df
Harsh Gupta <harsh@Harshs-Macbook-Pro.local>
2019-07-14 12:22:42 +0530

    Post Update and Delete with new schema

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,11 +11,11 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    numbers_sum=0
+    # numbers_sum=0
     posts = Post.query.all()
-    for post in posts:
-        numbers_sum = numbers_sum + post.numbers_issued
-    return render_template('home.html', posts=posts, numbers_sum=numbers_sum)
+    # for post in posts:
+        # numbers_sum = numbers_sum + post.numbers_issued
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
@@ -26,10 +26,6 @@ def about():
     text_about = """
     If Led Zeppelin were the band most responsible for hard rock's vertical expansion in the '70s, hitting previously unforeseeable heights for the genre, Pink Floyd were the band that expanded it the most horizontally.
 Obviously, they stretched out the length -- double albums, side-long jams, songs that had more movements and ideas than entire LPs by other bands. But they also broadened the music's width, with one of the most far-reaching musical palettes of any band approaching their magnitude. Starting with the Syd Barrett-stewarded kaleidoscopic psychedelia Piper at the Gates of Dawn in 1967 -- a half-century old this Saturday (Aug. 5) -- the band showed a truly staggering artistic flexibility and open-eared inventiveness, for which they remain oddly underrated in an era that increasingly views them as stodgy, cerebral rock puritans.
-
-Yes, they set the standard for college-dorm stoner rock with the prismatic prog of The Dark Side of the Moon, but in between the LP's space-rock zone-outs are a pulse-racing proto-EDM instrumental, a heart-stopping soul vocal exorcism and a couple ripping sax solos. Yes, Wish You Were Here is overwhelmed by a combined 26 minutes and nine movements of jazzy art-funking (and no shortage of fretting about The Machine), but it's also centered around the profound humanity of one of the great tear-jerking ballads in rock history. Yes, the '77 punk movement largely followed in response to the overblown pomposity of their ilk, but play Never Mind the Bollocks, Here's the Sex Pistols and Animals back to back and see which one sounds more like a bilious screed from a bunch of pissed-off Britons who don't give a f--k what their fans want to hear. And yes, The Wall was a monstrous double-LP statement of egomania from which there was no returning, but the set's rock operatics couldn't obscure the most seamless integration of disco's thump that any major rock band had yet achieved -- resulting in a Hot 100 No. 1 rock fans didn't even bother to cry "sell out!" over.
-
-
     """
     return render_template('about.html', title='About', text_about=text_about)
 
@@ -129,7 +125,7 @@ def new_post():
 def post(post_id):
     # get_or_404 is a method that gives a 404 if the post does not exist
     post = Post.query.get_or_404(post_id)
-    return render_template('post.html', title=post.title, post=post)
+    return render_template('post.html', title=post.material_id, post=post)
 
 
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
@@ -141,14 +137,24 @@ def update_post(post_id):
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
-        post.title = form.title.data
-        post.content = form.content.data
+        post.material_id = form.material_id.data
+        post.type_issued = form.type_issued.data
+        post.numbers_issued = form.numbers_issued.data
+        post.unit = form.unit.data
+        post.person = form.person.data
+        post.location = form.location.data
+
         db.session.commit()
         flash("Your post has been updated", 'success')
         return redirect(url_for('post', post_id=post_id))
     elif request.method == 'GET':
-        form.title.data = post.title
-        form.content.data = post.content
+        form.material_id.data = post.material_id
+        form.type_issued.data = post.type_issued
+        form.numbers_issued.data = post.numbers_issued
+        form.unit.data = post.unit
+        form.person.data = post.person
+        form.location.data = post.location
+
 
     return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
 
